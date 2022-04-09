228d4ab9d87ce0bd3f5669a80e6c760cc0d7b31f
Warmfilly <warm.filly@protonmail.com>
2022-04-08 22:06:33 -0600

    Add SRL abstract class

---
+++ src/Abstract Classes/SRL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdTool
+{
+    public abstract class SRL : Node
+    {
+        private string _send;
+        private string _receive;
+        private string _label;
+
+        public SRL(int X, int Y) : base(X, Y)
+        {
+            _send       = "empty";
+            _receive    = "empty";
+            _label      = "empty";
+        }
+
+        public string Send
+        {
+            get =>  _send;
+            set {   _send = SanitizeSRL(value); }
+        }
+
+        public string Receive
+        {
+            get =>  _receive;
+            set {   _receive = SanitizeSRL(value); }
+        }
+
+        public string Label
+        {
+            get =>  _label;
+            set {   _label = SanitizeSRL(value); }
+        }
+
+        //Used to ensure the Send, Receive, and Label properties of some PD objects have values that are considered valid by PD
+        //spaces replaced by underscores, etc.
+        private static string SanitizeSRL(string s)
+        {
+            s = s.Trim();
+            return String.IsNullOrWhiteSpace(s) ? "empty" : s.Replace(' ', '_');
+        }
+
+    }
+}
